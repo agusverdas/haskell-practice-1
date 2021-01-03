3c19774599103df54e488161c70bac3a2a2662a2
agusverdas <alexandergusakovsky@gmail.com>
2021-01-03 17:45:02 +0300

    Change local git user

--- Tasks.hs
+++ Tasks.hs
@@ -280,3 +280,8 @@ module Tasks where
 
   logEntryToString :: LogEntry -> String
   logEntryToString (LogEntry time level mes) = (timeToString time) ++ ": " ++ (logLevelToString level) ++ ": " ++ mes
+
+  data Person = Person { firstName :: String, lastName :: String, age :: Int }
+
+  updateLastName :: Person -> Person -> Person
+  updateLastName person1 person2 = person2 { lastName = lastName person1 }
